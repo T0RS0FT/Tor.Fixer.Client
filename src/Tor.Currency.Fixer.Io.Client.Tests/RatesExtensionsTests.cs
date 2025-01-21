6593c9491c9cc7977c68ca9d895963a3ea10bed7
T0RS0FT <balazs.toronyi@gmail.com>
2025-01-21 15:38:32 +0100

    RatesExtensions + unit tests

---
+++ src/Tor.Currency.Fixer.Io.Client.Tests/RatesExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using Tor.Currency.Fixer.Io.Client.Extensions;
+using Tor.Currency.Fixer.Io.Client.Models;
+
+namespace Tor.Currency.Fixer.Io.Client.Tests
+{
+    [TestClass]
+    public class RatesExtensionsTests
+    {
+        [DataTestMethod]
+        [DataRow("EUR", "EUR", 1, true, 1)]
+        [DataRow("eur", "EUR", 1, true, 1)]
+        [DataRow("EUR", "eur", 1, true, 1)]
+        [DataRow("eur", "eur", 1, true, 1)]
+        [DataRow("EUR", "EUR", 10, true, 10)]
+        [DataRow("EUR", "USD", 1, true, 1.04)]
+        [DataRow("EUR", "USD", 2, true, 2.08)]
+        [DataRow("USD", "EUR", 1, true, 0.96)]
+        [DataRow("USD", "EUR", 5, true, 4.8)]
+        [DataRow("USD", "GBP", 1, true, 0.81)]
+        [DataRow("USD", "GBP", 10, true, 8.17)]
+        [DataRow("USD", "USB", 1, false, 0)]
+        [DataRow("USB", "USC", 1, false, 0)]
+        [DataRow("USB", "USD", 1, false, 0)]
+        [DataRow("", "USD", 1, false, 0)]
+        [DataRow("USD", "", 1, false, 0)]
+        [DataRow("", "", 1, false, 0)]
+        [DataRow(null, "USD", 1, false, 0)]
+        [DataRow("USD", null, 1, false, 0)]
+        [DataRow(null, null, 1, false, 0)]
+        [DataRow("  ", "USD", 1, false, 0)]
+        [DataRow("USD", "   ", 1, false, 0)]
+        [DataRow("  ", "    ", 1, false, 0)]
+        public void RatesExtensionsConvertTest(
+            string sourceCurrencyCode,
+            string destinationCurrencyCode,
+            double amount,
+            bool success,
+            double expectedResult)
+        {
+            var rates = new LatestRates()
+            {
+                BaseCurrencyCode = "EUR",
+                Rates =
+                [
+                    new CurrencyRate(){ CurrencyCode="USD", ExchangeRate=(decimal)1.04 },
+                    new CurrencyRate(){ CurrencyCode="GBP", ExchangeRate=(decimal)0.85 }
+                ]
+            };
+
+            try
+            {
+                var result = rates.Convert(sourceCurrencyCode, destinationCurrencyCode, (decimal)amount);
+
+                Assert.IsTrue(Math.Abs((decimal)expectedResult - result) < 0.01m);
+            }
+            catch (Exception ex)
+            {
+                if (success)
+                {
+                    Assert.Fail(ex.Message);
+                }
+            }
+        }
+    }
+}
--- src/Tor.Currency.Fixer.Io.Client/Extensions/RatesExtensions.cs
+++ src/Tor.Currency.Fixer.Io.Client/Extensions/RatesExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using Tor.Currency.Fixer.Io.Client.Interfaces;
+using Tor.Currency.Fixer.Io.Client.Models;
 
 namespace Tor.Currency.Fixer.Io.Client.Extensions
 {
-    // TODO unit tests
     public static class RatesExtensions
     {
         public static decimal Convert(
@@ -11,6 +11,15 @@ namespace Tor.Currency.Fixer.Io.Client.Extensions
             string destinationCurrencyCode,
             decimal quantity)
         {
+            ArgumentNullException.ThrowIfNull(rates);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(sourceCurrencyCode);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(destinationCurrencyCode);
+
+            if (sourceCurrencyCode.Equals(destinationCurrencyCode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return quantity;
+            }
+
             if (sourceCurrencyCode.Equals(rates.BaseCurrencyCode, StringComparison.InvariantCultureIgnoreCase))
             {
                 var rate = rates.Rates.SingleOrDefault(x => x.CurrencyCode.Equals(destinationCurrencyCode, StringComparison.InvariantCultureIgnoreCase));
@@ -37,6 +46,66 @@ namespace Tor.Currency.Fixer.Io.Client.Extensions
                 : destinationRate == null
                     ? throw new Exception("Destination currency code not found")
                     : destinationRate.ExchangeRate / sourceRate.ExchangeRate * quantity;
+        }
+
+        // TODO unit tests
+        public static LatestRates ChangeBaseCurrency(this LatestRates rates, string baseCurrencyCode)
+        {
+            ArgumentNullException.ThrowIfNull(rates);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(baseCurrencyCode);
+
+            return new LatestRates()
+            {
+                Date = rates.Date,
+                BaseCurrencyCode = baseCurrencyCode.ToUpper(),
+                Timestamp = rates.Timestamp,
+                Rates = RecalculateRates(rates, baseCurrencyCode)
+            };
+        }
+
+        // TODO unit tests
+        public static HistoricalRates ChangeBaseCurrency(this HistoricalRates rates, string baseCurrencyCode)
+        {
+            ArgumentNullException.ThrowIfNull(rates);
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(baseCurrencyCode);
+
+            return new HistoricalRates()
+            {
+                Date = rates.Date,
+                BaseCurrencyCode = baseCurrencyCode.ToUpper(),
+                Timestamp = rates.Timestamp,
+                Historical = rates.Historical,
+                Rates = RecalculateRates(rates, baseCurrencyCode)
+            };
+        }
+
+        private static List<CurrencyRate> RecalculateRates(IRates rates, string baseCurrencyCode)
+        {
+            if (rates.BaseCurrencyCode.Equals(baseCurrencyCode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return rates.Rates.Select(x => new CurrencyRate()
+                {
+                    CurrencyCode = x.CurrencyCode,
+                    ExchangeRate = x.ExchangeRate
+                }).ToList();
+            }
+
+            var baseRate = rates.Rates.SingleOrDefault(x => x.CurrencyCode.Equals(baseCurrencyCode, StringComparison.InvariantCultureIgnoreCase))
+                ?? throw new Exception("Currency code not found");
+
+            return [.. rates.Rates
+                .Select(x => x.CurrencyCode.Equals(baseCurrencyCode, StringComparison.InvariantCultureIgnoreCase)
+                    ? new CurrencyRate()
+                    {
+                        CurrencyCode = rates.BaseCurrencyCode,
+                        ExchangeRate = 1 / x.ExchangeRate
+                    }
+                    : new CurrencyRate()
+                    {
+                        CurrencyCode = x.CurrencyCode,
+                        ExchangeRate = baseRate.ExchangeRate / x.ExchangeRate
+                    })
+                .OrderBy(x => x.CurrencyCode)];
         }
     }
 }
